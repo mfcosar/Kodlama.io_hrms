d79f27c67a4d47568d934365654c5c2a2f035e25
Feyza Coşar <mfcosar@hotmail.com>
2024-08-06 20:22:05 +0300

    Swagger 3 desteği API'ye eklendi

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -2,6 +2,10 @@ package kodlamaio.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -9,5 +13,13 @@ public class HrmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
+	
+	@Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("hrms")      
+                                 .description("Your API Description")
+                                 .version("1.0"));
+    }
 
 }
