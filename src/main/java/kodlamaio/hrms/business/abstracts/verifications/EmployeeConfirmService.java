9c7e579d3f370ddec8e89b88b6a8410ec5362b82
Feyza Coşar <mfcosar@hotmail.com>
2024-08-11 16:57:37 +0300

    Api katmanına CandidatesController, EmployersController eklendi.

--- src/main/java/kodlamaio/hrms/business/abstracts/verifications/EmployeeConfirmService.java
+++ src/main/java/kodlamaio/hrms/business/abstracts/verifications/EmployeeConfirmService.java
@@ -3,13 +3,12 @@ package kodlamaio.hrms.business.abstracts.verifications;
 import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Employee;
 import kodlamaio.hrms.entities.concretes.Employer;
-import kodlamaio.hrms.entities.concretes.verifications.EmployeeConfirm;
 
 
 public interface EmployeeConfirmService {
 
-	Result sendEmployeeConfirmation(Employer employer);
-	Result confirmEmployer(EmployeeConfirm employeeConfirm, Employee employee);
-	EmployeeConfirm getOne(int employerId);
+	Result generateEmployeeConfirmation(Employer employer);
+	Result confirmEmployer(int employeeConfirmId, Employee employee);
+
 }
 
